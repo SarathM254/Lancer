9a669c8760aa2b6d9411cbe15ba8037726bcd9e7
M.Sarath <motupallisarathchandra@gmail.com>
2026-01-19 19:48:31 +0530

    Express and dependancies setup

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,7 +8,7 @@ import Messages from './Messages.jsx'
 const Profile = () => {
     return (
         <div>
-            <div class="Profile-content flex-1 flex space-x-9">
+            <div class="Profile-content flex-1 flex flex-col space-x-9 xl:flex-row">
                 <div class="flex-[4] basis-0 flex flex-col gap-4 justify-center">
 
                     <Basic></Basic>
